faf9d857265fe45ba3fc2f4d187d1d5a1bbc7051
lhyXZC <lhyXZC@gmail.com>
2025-04-12 20:36:59 +0200

    move-variable.py

---
+++ practice/variable.py
@@ -0,0 +1,13 @@
+message = "Hello"
+print(message)
+message = "aaa"#更新变量存储的内容
+print(message)
+
+print(message.title())#打印的字符串首字母大写
+print(message.upper())
+print(message.lower())
+print(f"User_name {message.lower()}")#  f"字{变量}符"   通过这种格式，在字符串中用变量
+
+id=1145145
+message = f"\tID:\n{id}"
+print(message)
--- variable.py
+++
@@ -1,8 +0,0 @@
-message = "Hello"
-print(message)
-message = "Say hello again"
-print(message)
-
-print(message.title())
-print(message.upper())
-print(message.lower())
